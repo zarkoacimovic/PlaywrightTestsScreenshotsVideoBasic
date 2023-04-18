c0c4c059ccc6bcd4000018a963c074128d714f8d
Zarko Acimovic <zarko.acimovic@endava.com>
2023-04-18 16:35:12 +0200

    start tracing for page context

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -18,7 +18,7 @@ public class Tests : PageTest
     [Test]
     public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
     {
-        await Page.GotoAsync("https://playwright.dev");
+       await Page.GotoAsync("https://playwright.dev");
 		
 		var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
         //Browser
@@ -29,25 +29,15 @@ public class Tests : PageTest
 
         _page = await Browser.NewPageAsync();
 
-        context = await Browser.NewContextAsync(new BrowserNewContextOptions
+        await _page.Context.Tracing.StartAsync(new TracingStartOptions
         {
-            ViewportSize = new ViewportSize() { Width = 1920, Height = 1080 },
-            RecordVideoDir = "videos/",
-            AcceptDownloads = true,
-            ColorScheme = ColorScheme.Dark
-        });
-
-
-        await context.Tracing.StartAsync(new TracingStartOptions
-        {
-            Title = "zare23" + context.ToString(), //Note this is for MSTest only. 
+            Title = "zare43", //Note this is for MSTest only. 
             Screenshots = true,
             Snapshots = true,
             Sources = true
         });
 
-
-
+       
         _page.GotoAsync("http://www.eaapp.somee.com");
         Thread.Sleep(5000);
 
@@ -69,15 +59,11 @@ public class Tests : PageTest
 
         if (Browser != null)
         {
-            //context = await Browser.NewContextAsync();
-
-
-            await context.Tracing.StopAsync(new TracingStopOptions
+            await _page.Context.Tracing.StopAsync(new TracingStopOptions
             {
-                Path = "videos.zip" //+ context.ToString()
+                Path = "videos2.zip" //+ context.ToString()
             });
 
-            await context.CloseAsync();
             await Browser.CloseAsync();
         }
     }
